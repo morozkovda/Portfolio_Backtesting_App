cca51554e373f2cd92aed491ed407516490dacca
morozkovda <115310903+morozkovda@users.noreply.github.com>
2023-02-17 11:27:03 -0800

    finally working

--- backtest.py
+++ backtest.py
@@ -3,16 +3,13 @@ from pandas_datareader import data as pdr
 from datetime import datetime
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import copy
 import backtrader as bt
-import backtrader.feeds as btfeeds
 from pypfopt import HRPOpt, risk_models, expected_returns
 
 yf.pdr_override()
 
 #get data
-tickers = ['SPY','META','AAPL']
+tickers = ['SPY' ,'META', 'AAPL']
 stock = pdr.get_data_yahoo(tickers,
                 start= datetime(2015, 1, 1),
                 end= datetime(2023, 2, 10), interval = '1d')
@@ -39,20 +36,14 @@ class Model:
 
         model_n = param['model_n']
         isCleanWeight = param['isCleanWeight']
-        upd_period = param['upd_period']
-        isDataCollecting = param['collectData']
 
         mu = expected_returns.mean_historical_return(self.data).fillna(0.0).values
 
         S = risk_models.sample_cov(self.data).fillna(0.0).values
 
+        self.isFirst = False
+        self.data = data.copy()
 
-        # data = data.fillna(0.0)
-        if (self.isFirst == True) or (upd_period == 0) or (isDataCollecting == False):
-            self.isFirst = False
-            self.data = data.copy()
-        else:
-            self.data = pd.concat([self.data, data.iloc[-upd_period:]])
 
         # print(data.iloc[-upd_period:])
 
@@ -72,24 +63,23 @@ class optimizer(bt.SignalStrategy):
         ('DataCounter', 125),
         ('RebalanceDay', 22),
         ('reserveCash', 1000.0),
-        ('model_params', {
-        'model_n':'HRP',
-        'collectData':False,
-        'isCleanWeight':False,
-        'upd_period': 22}),
-        ('printlog', 0),
+        ('model_params',
+         { 'model_n':'HRP',
+        'isCleanWeight':False}
+         ),
+        ('printlog', 1),
         ('model', Model())
     )
 
     def getPosDifference(self, cash, alloc, new_price, cur_pos):
         pos_cash = new_price * cur_pos
-        print('pos_cash',pos_cash)
+        # print('pos_cash',pos_cash)
         all_cash = cash + np.sum(pos_cash)
-        print('all_cash',all_cash)
+        # print('all_cash',all_cash)
         cash_alloc = alloc * all_cash
-        print('cash_alloc',cash_alloc)
+        # print('cash_alloc',cash_alloc)
         new_pos = (cash_alloc / new_price).astype(int)
-        print('new_pos',new_pos)
+        # print('new_pos',new_pos)
         diff_pos = cur_pos - new_pos
         return diff_pos * (-1)
 
@@ -126,7 +116,6 @@ class optimizer(bt.SignalStrategy):
         self.nDatas = len(self.datas)
         self.DataCounter = self.params.DataCounter  # 125
         self.RebalanceDay = self.params.RebalanceDay  # 22
-        self.reserveCash = self.params.reserveCash  # 1000.0
         self.verbose = self.params.printlog  # 0
         self.model = self.params.model
         self.model_params = self.params.model_params
@@ -140,44 +129,37 @@ class optimizer(bt.SignalStrategy):
         m_input = self.getModelDataFrame()
         if self.update_counter == 0:
             self.new_pct = np.round(self.model.get_allocations(m_input.values, **self.model_params), 2)
-            print(self.isFirst)
-            if self.isFirst == True:
-                self.old_pct = np.zeros(len(self.new_pct))
-                # if np.array_equal(self.new_pct, self.old_pct) == False:
-                #     if self.verbose > 1:
-                #         print('size', self.getPosSize().tolist())
-                #         print('price', self.getPosOpenPrice().tolist())
-                #     if self.verbose > 0:
-                #         print("rebalance new percent.",self.new_pct)
+            self.old_pct = np.zeros(len(self.new_pct))
 
-                cash = self.broker.get_cash()
-                new_price = self.getCurrentClosePrice()
-                cur_pos = self.getPosSize()
-                upd_pos = self.getPosDifference(cash, self.new_pct, new_price, cur_pos)
-                to_sell = []
-                to_buy = []
-                for i, p in enumerate(upd_pos):
-                    if (p < 0):
-                        to_sell.append((i, p))
-                    elif (p > 0):
-                        to_buy.append((i, p))
+            if self.verbose == 1:
+                print('Current portfolio value %s' % self.broker.getvalue())
+                print('Weights for current epoch %s' % self.new_pct)
 
-                for i, p in to_sell:
-                    if self.verbose > 0:
-                        print('sell', i, p)
-                    self.sell(self.datas[i], p)
-                for i, p in to_buy:
-                    if self.verbose > 0:
-                        print('buy', i, p)
-                    self.buy(self.datas[i], p)
+            cash = self.broker.get_cash()
+            new_price = self.getCurrentClosePrice()
+            cur_pos = self.getPosSize()
+            upd_pos = self.getPosDifference(cash, self.new_pct, new_price, cur_pos)
+            to_sell = []
+            to_buy = []
+            for i, p in enumerate(upd_pos):
+                if (p < 0):
+                    to_sell.append((i, p))
+                elif (p > 0):
+                    to_buy.append((i, p))
 
-                self.old_pct = self.new_pct
+            for i, p in to_sell:
+                if self.verbose > 0:
+                    print('sell', i, p)
+                self.sell(self.datas[i], p)
+            for i, p in to_buy:
+                if self.verbose > 0:
+                    print('buy', i, p)
+                self.buy(self.datas[i], p)
 
+            self.old_pct = self.new_pct
         self.update_counter += 1
         if self.update_counter == self.RebalanceDay:
             self.update_counter = 0
-
-
 
 
 
@@ -195,7 +177,7 @@ cerebro.addstrategy(optimizer)
 thestrats = cerebro.run()
 thestrat = thestrats[0]
 
-# cerebro.plot()
+cerebro.plot()
 
 print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
